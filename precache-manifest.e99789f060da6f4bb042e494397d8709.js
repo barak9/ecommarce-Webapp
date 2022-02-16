fecaa38dd4e8d847484d10f400835f7b3275632f
unknown <pkparveenbarak@gmail.com>
2022-02-16 10:33:35 +0530

    Updates

---
+++ precache-manifest.e99789f060da6f4bb042e494397d8709.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e2d82688f3a4de530a71d719a50087e",
+    "url": "/ecommarce-Webapp/index.html"
+  },
+  {
+    "revision": "1a5c3a79abcf978bf81e",
+    "url": "/ecommarce-Webapp/static/css/main.e3376123.chunk.css"
+  },
+  {
+    "revision": "34bdfda3ba69cc4135ac",
+    "url": "/ecommarce-Webapp/static/js/2.5353cd36.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/ecommarce-Webapp/static/js/2.5353cd36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a5c3a79abcf978bf81e",
+    "url": "/ecommarce-Webapp/static/js/main.5712f96a.chunk.js"
+  },
+  {
+    "revision": "ed3d61cccc5764d70768",
+    "url": "/ecommarce-Webapp/static/js/runtime-main.64ab15c8.js"
+  },
+  {
+    "revision": "eef9ed5d3cbbda49ccc51a74e51637d4",
+    "url": "/ecommarce-Webapp/static/media/acer-desktop.eef9ed5d.jpg"
+  },
+  {
+    "revision": "51dd98e6975e7af9797b7cdd20ba1f06",
+    "url": "/ecommarce-Webapp/static/media/acer.51dd98e6.jpg"
+  },
+  {
+    "revision": "a6a3c66363bbb5bb5f6ac5f43eb36d76",
+    "url": "/ecommarce-Webapp/static/media/background.a6a3c663.jpg"
+  },
+  {
+    "revision": "2a17d395a626aee7ec7962013dc038db",
+    "url": "/ecommarce-Webapp/static/media/dell-desktop.2a17d395.jpg"
+  },
+  {
+    "revision": "2066e320fd9ece09d085c8123c8a744f",
+    "url": "/ecommarce-Webapp/static/media/dell.2066e320.jpg"
+  },
+  {
+    "revision": "6702203991ed0167f93c438934691be8",
+    "url": "/ecommarce-Webapp/static/media/headphone.67022039.jpg"
+  },
+  {
+    "revision": "63c2997dd20f9f38cb36afd220d123c3",
+    "url": "/ecommarce-Webapp/static/media/lanavo.63c2997d.jpg"
+  },
+  {
+    "revision": "9f359ca65b833f29bf7a671598f17a19",
+    "url": "/ecommarce-Webapp/static/media/mike.9f359ca6.jpg"
+  },
+  {
+    "revision": "b11122f09deb91b24bd8b186e1400ef6",
+    "url": "/ecommarce-Webapp/static/media/ring.b11122f0.jpg"
+  }
+]);
